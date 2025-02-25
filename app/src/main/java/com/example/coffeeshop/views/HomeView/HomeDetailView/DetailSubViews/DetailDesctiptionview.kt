bc4583251be80208d554e4936a234f45d645aebc
Maxim Svidrak <maximsvid@gmail.com>
2025-02-25 15:35:07 +0100

    weiter

--- app/src/main/java/com/example/coffeeshop/views/HomeView/HomeDetailView/DetailSubViews/DetailDesctiptionview.kt
+++ app/src/main/java/com/example/coffeeshop/views/HomeView/HomeDetailView/DetailSubViews/DetailDesctiptionview.kt
@@ -7,11 +7,19 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.LocationOn
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.focusModifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @SuppressLint("DefaultLocale")
 @Composable
@@ -54,15 +62,31 @@ fun DetailDescriptionView (
             horizontalArrangement = Arrangement.SpaceBetween
         ){
             Row (
-
+                verticalAlignment = Alignment.CenterVertically
             ){
-
+                Icon (
+                  imageVector = Icons.Default.LocationOn,
+                    contentDescription = "Region",
+                    modifier = Modifier.size(18.dp)
+                )
+                Spacer(modifier = Modifier.width(4.dp))
+                Text(
+                    text = region,
+                    style = MaterialTheme.typography.headlineSmall
+                )
             }
-            Text(text = region)
-
-            Text(text = roastLevel.toString())
+        }
+        
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+            Text(text = "Roast level: ${roastLevel.toString()}")
         }
 
-        Text(text = productDescription)
+        Text(
+            text = productDescription,
+            style = MaterialTheme.typography.bodyMedium
+        )
     }
 }
