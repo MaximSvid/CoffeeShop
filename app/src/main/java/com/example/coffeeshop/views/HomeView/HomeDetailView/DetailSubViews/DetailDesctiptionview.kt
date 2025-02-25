fd00b0a7bf22258b2a513f1b01cb2050a9704f3b
Maxim Svidrak <maximsvid@gmail.com>
2025-02-25 11:58:00 +0100

    deteil weiter...

---
+++ app/src/main/java/com/example/coffeeshop/views/HomeView/HomeDetailView/DetailSubViews/DetailDesctiptionview.kt
@@ -0,0 +1,13 @@
+package com.example.coffeeshop.views.HomeView.HomeDetailView.DetailSubViews
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun DetailDescriptionView (
+    name: String,
+    productDescription: String,
+    price: Double,
+    region: String
+) {
+
+}
